068fa2a0fed978f1ef52672b3a2ab7f803459e35
Raichev Dmitry <raichevdp@gmail.com>
2019-04-23 00:32:30 +0300

    Initial commit

---
+++ src/main/java/com/raichevdima/carSeller/CarSellerApplication.java
@@ -0,0 +1,13 @@
+package com.raichevdima.carSeller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarSellerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarSellerApplication.class, args);
+	}
+
+}
